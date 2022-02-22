3fba57abd925b7cc983d501ba77b212704d3ee01
mirror2005 <98074021+mirror2005@users.noreply.github.com>
2022-02-22 09:35:18 +0530

    Update set_watermark_text.py

--- bot/plugins/set_watermark_text.py
+++ bot/plugins/set_watermark_text.py
@@ -16,7 +16,7 @@ async def _(c, m):
             if user.status == "kicked":
                 await c.send_message(
                    chat_id=m.chat.id,
-                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                   text="Sorry Sir, You are Banned to use me. Contact my [Channel](https://t.me/jetbots).",
                    parse_mode="markdown",
                    disable_web_page_preview=True
                 )
@@ -38,7 +38,7 @@ async def _(c, m):
         except Exception:
             await c.send_message(
                 chat_id=m.chat.id,
-                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+                text="Something went Wrong. Contact my [Channel](https://t.me/jetbots).",
                 parse_mode="markdown",
                 disable_web_page_preview=True)
             return
